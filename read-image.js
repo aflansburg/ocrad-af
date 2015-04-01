b463ed7386d4584a08dc2fc6cc384a5c2298a0db
aflansburg <abram@markitx.com>
2015-04-01 16:03:32 -0500

    af alteration of OCRAD initial

--- read-image.js
+++ read-image.js
@@ -1,8 +1,17 @@
-var OCRAD = require('/Users/abram/Projects/af/ocrad/ocrad.js');
+var OCRAD = require('./ocrad.js');
 var Canvas = require('canvas');
 var Image = Canvas.Image;
 var fs = require('fs');
-var fp = process.argv.slice(2);
+var fp = process.argv[2];
+var argv = require('minimist')(process.argv.slice(2), {
+    'alias': {
+        'key': '_'
+    },
+    'string': ['out'],
+});
+
+outPath = argv.out ? argv.out : '/dev/null';
+console.log('Saving output to ' + outPath);
 
 fs.readFile(String(fp), function(err, src) {
   if (err) {
@@ -14,4 +23,7 @@ fs.readFile(String(fp), function(err, src) {
   var ctx = canvas.getContext('2d');
   ctx.drawImage(img, 0, 0, img.width, img.height);
   console.log(OCRAD(canvas));
+  if (!fs.existsSync(outPath)) {
+  	fs.writeFileSync(outPath, OCRAD(canvas));
+  }
 });
